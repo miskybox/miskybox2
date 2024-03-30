c9a07a4e4f178bb4b8e9810fe7acc294ec7e00e2
SiGu_Dev <139983709+miskybox@users.noreply.github.com>
2024-03-30 23:13:11 +0100

    Añadir .gitignore

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Eva from "./read";
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Eva />
+    </>
+  );
+}
+
+export default App;
